015b4dbfcdda284e837d07613095040fbeecd379
Zhang Gavin <rotblade@163.com>
2015-12-10 18:16:51 +0800

    ES action function

--- data2es.py
+++ data2es.py
@@ -4,27 +4,27 @@ import click
 
 
 def echo(message, quiet):
-    '''
+    """
     Print the given message to standard out via click unless quiet is True.
 
     :param message: the message to print out
     :param quiet: don't print the message when this is True
-    '''
+    """
     if not quiet:
         click.echo(message)
 
 
 def str_to_esfield(raw_str):
-    '''
+    """
     Return a string that meets the field name requirements in ES.
 
     :param raw_str: the string to be converted
-    '''
+    """
     def f(c):
-        '''
+        """
         Remove all characters except alphabetic, space, hyphen
         and underscore
-        '''
+        """
         if c.isalpha() or c in [' ', '-', '_']:
             return c
         else:
@@ -32,7 +32,6 @@ def str_to_esfield(raw_str):
 
     new_str = raw_str.strip()
     new_str = ''.join(map(f, new_str))
-
     # use one '_' to replace successive spaces or hyphenes
     for i in [' ', '-']:
         if i in new_str:
@@ -41,15 +40,33 @@ def str_to_esfield(raw_str):
     return new_str.lower()
 
 
-def docs_from_file(es, filename, delimiter, quiet):
-    '''
+def index_op(doc, meta):
+    """
+    Return a document-indexing operation that can be passed to
+    'bulk' method.
+
+    :arg doc: A mapping of fields
+    :arg meta: A mapping of underscore-prefixed fields with special
+        meaning to ES, like ``_id`` and ``_type``
+    """
+    def underscore_keys(d):
+        """Return a dict with every key prefixed by an underscore."""
+        return dict(('_%s' % k, v) for k, v in d.items())
+
+    action = underscore_keys(meta)
+    action['_source'] = doc
+    return action
+
+
+def docs_from_file(filename, idx_name, doc_type, quiet):
+    """
     Return a generator for pulling rows from a given delimited file.
 
-    :param es: an ElasticSearch client
     :param filename: the name of the file to read from or '-' if stdin
-    :param delimiter: the delimiter to use
+    :param idx_name: index name
+    :param doc_type: document type
     :param quiet: don't output anything to the console when this is True
-    '''
+    """
     def all_docs():
         with open(filename, newline='') as doc_file:
             reader_obj = csv.reader(doc_file)
@@ -59,7 +76,14 @@ def docs_from_file(es, filename, delimiter, quiet):
             for field in fields:
                 echo(field, quiet)
 
-            dict_reader = csv.DictReader(doc_file, fields)
+            dict_reader = csv.DictReader(doc_file, fieldnames=fields)
+            for row in dict_reader:
+                meta = {
+                    'index': idx_name,
+                    'type': doc_type,
+                    'id': row[fields[0]]
+                }
+                yield index_op(row, meta)
 
     return all_docs
 
--- test_data2es.py
+++ test_data2es.py
@@ -1,6 +1,6 @@
 import csv
 import unittest
-from data2es import str_to_esfield
+from data2es import str_to_esfield, index_op
 
 
 class TestOneStringFun(unittest.TestCase):
@@ -18,7 +18,7 @@ class TestTwoCsvFile(unittest.TestCase):
 
 
     def test_1_header(self):
-        header = ['user_upi', 'name', 'work_title', 'job_desc']
+        header = ['ticketno', 'name', 'work_title', 'job_desc']
         reader_obj = csv.reader(self.doc_file)
         # delimited file should include the field names as the first row
         fields = [str_to_esfield(item) for item in next(reader_obj)]
@@ -27,7 +27,7 @@ class TestTwoCsvFile(unittest.TestCase):
 
     def test_2_row(self):
         dict_row = {
-            'user_upi': '10002',
+            'ticketno': '10002',
             'name': 'Zhang Gavin',
             'job_desc': 'aaaa_',
             'work_title': 'supervisor'
@@ -37,6 +37,28 @@ class TestTwoCsvFile(unittest.TestCase):
         dict_reader = csv.DictReader(self.doc_file, fields)
         self.assertEqual(next(dict_reader), dict_row)
 
+    def test_3_action(self):
+        dict_action = {
+            '_index': 'qd',
+            '_type': 'ticket',
+            '_id': '10002',
+            '_source': {
+                'ticketno': '10002',
+                'name': 'Zhang Gavin',
+                'job_desc': 'aaaa_',
+                'work_title': 'supervisor'
+            }
+        }
+        reader_obj = csv.reader(self.doc_file)
+        fields = [str_to_esfield(item) for item in next(reader_obj)]
+        dict_reader = csv.DictReader(self.doc_file, fields)
+        row = next(dict_reader)
+        meta = {
+            'index': 'qd',
+            'type': 'ticket',
+            'id': row[fields[0]]
+        }
+        self.assertEqual(index_op(row, meta), dict_action)
 
 if __name__ == '__main__':
     unittest.main()
